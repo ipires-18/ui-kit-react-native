428817f0ad848173d4b9bc446fe017328d173446
Ian Oliveira <ian.oliveira@einstein.br>
2023-07-21 10:38:55 -0300

    feat: config the icons for ios e android

--- packages/components/Icons/index.tsx
+++ packages/components/Icons/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text } from 'react-native'
 import type { IconsProps } from './src/types/icons.types'
+
+import IconCam from './assets/camera.svg'
 
 export const Icons: React.FC<IconsProps> = ({ title = 'Template básico' }) => {
   return (
-    <View>
+    <>
       <Text>{title}</Text>
-    </View>
+       <IconCam  fill="red" width={100} height={100} />
+    </>
   )
 }
